9b67f1ba99b0b9241ed0afee047345bf9c2e2975
Derwin McGeary <derwinmcgeary+web@gmail.com>
2016-03-18 10:18:04 +0000

    tweak slightly

--- answers.sh
+++ answers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd ~
-echo "Files with u+x permission in HOME"
+echo "Files with x permission in HOME"
 ls -l | grep "^...x" | wc -l
 
 echo "Launching Google Chrome..."
 google-chrome && echo "Google Chrome has this many processes"
-ps aux | grep chrome | wc -l
+ps aux | grep chrome | grep -v grep | wc -l
 
 echo "Users with number > 100 in /etc/passwd:"
 cat /etc/passwd | grep ":[[:digit:]]\{3,\}:" | cut -d ':' -f 1 | sed -e's/\n/, /'
